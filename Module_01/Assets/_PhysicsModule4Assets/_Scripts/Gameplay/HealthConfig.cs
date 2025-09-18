f041fb77f1c34ede35804034c6ef53d1dc2fc857
San Diego Mesa College <sdmesasoftware@sdccd.edu>
2025-09-17 20:28:09 -0700

    adjusting Folder
    deleted module4assets folder because the changes were populated already

--- Module_01/Assets/_PhysicsModule4Assets/_Scripts/Gameplay/HealthConfig.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "HealthConfig", menuName = "Scriptable Objects/HealthConfig")]
-public class HealthConfig : ScriptableObject
-{
-    public int maxHealth = 100;
-    public int startHealth = 100;
-}
